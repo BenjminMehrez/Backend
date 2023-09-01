6f872bbe349265ec45b3b23bc5c1f13089e00a39
Benjamin Mehrez <benjaminjofre01@gmail.com>
2023-09-01 17:38:58 -0300

    cerrando realtime

--- src/public/js/realTimeProducts.js
+++ src/public/js/realTimeProducts.js
@@ -1,79 +1,76 @@
-const socket = io();
+const socketClient = io()
 
 
-
-socketClient.on("enviodeproducts",(obj)=>{
-  updateProductList(obj)
+socketClient.on("enviodeproducts", (obj) => {
+    updateProductList(obj)
 })
 
 
 function updateProductList(products) {
-  let div = document.getElementById("list-products");
-  let productos = "";
+    let div = document.getElementById("list-products");
+    let productos = "";
 
-  products.forEach((product) => {
-    productos += `
-    <div class="container">
-    <div class="caja">
+    products.forEach((product) => {
+        productos += `
+        <div class="container">
+        <div class="caja">
         <div><span>ID: </span>
             <p>${product.id}</p>
-        </div>
-        <div><span>Nombre: </span>
+            </div>
+            <div><span>Nombre: </span>
             <p>${product.title}</p>
-        </div>
-        <div><span>Precio: </span>
-            <p>${product.price}</p>
-        </div>
-        <div><span>Descripcion: </span>
+            </div>
+            <div><span>Descripcion: </span>
             <p>${product.description}</p>
-        </div>
-        <div><span>Thumbnails: </span>
+            </div>
+            <div><span>Precio: </span>
+            <p>${product.price}</p>
+            </div>
+            <div><span>Thumbnails: </span>
             <p>${product.thumbnails}</p>
-        </div>
-        <div><span>Status: </span>
+            </div>
+            <div><span>Status: </span>
             <p>${product.status}</p>
-        </div>
-        <div><span>Stock: </span>
+            </div>
+            <div><span>Stock: </span>
             <p>${product.stock}</p>
+            </div>
         </div>
-    </div>
+    </div>`;
+    });
 
-</div>
-        
-        `;
-  });
-
-  div.innerHTML = productos;
+    div.innerHTML = productos;
 }
 
 
 let form = document.getElementById("formulario");
 form.addEventListener("submit", (evt) => {
-evt.preventDefault();
+    evt.preventDefault();
 
-let title = form.elements.title.value;
-let description = form.elements.description.value;
-let price = form.elements.price.value;
-let thumbnail = form.elements.thumbnail.value;
-let code = form.elements.code.value;
-let stock = form.elements.stock.value;
+    let title = form.elements.title.value;
+    let description = form.elements.description.value;
+    let price = form.elements.price.value;
+    let thumbnail = form.elements.thumbnail.value;
+    let code = form.elements.code.value;
+    let stock = form.elements.stock.value;
 
-socketClient.emit("addProduct", {
-  title,
-  description,
-  stock,
-  price,
-  thumbnail,
-  code,
-});
+    socketClient.emit("addProduct", {
+        title,
+        description,
+        price,
+        thumbnail,
+        code,
+        stock,
+    });
 
-form.reset();
+    form.reset();
 });
 
 document.getElementById("delete-btn").addEventListener("click", function () {
-  const deleteidinput = document.getElementById("id-prod");
-  const deleteid = parseInt(deleteidinput.value);
-  socketClient.emit("deleteProduct", deleteid);
-  deleteidinput.value = "";
-})
+    const deleteidinput = document.getElementById("id-prod");
+    const deleteid = parseInt(deleteidinput.value);
+    socketClient.emit("deleteProduct", deleteid);
+    deleteidinput.value = "";
+});
+
 
