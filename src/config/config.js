ef0389199d83085780a7ebf8777a9b7be2312243
Benjamin Mehrez <benjaminjofre01@gmail.com>
2023-10-31 17:22:50 -0300

    update

--- src/config/config.js
+++ src/config/config.js
@@ -1,18 +1,28 @@
-export const config = {
-    server:{
-        port:8080,
-        secretSession:"CoderSecret"
-    },
-    fileSystem:{
-        productsFile:"products.json",
-        cartFile:"carts.json"
-    },
-    mongo:{
-        url:"mongodb+srv://PracticaIntegradora:12344@cursobackend.aoxi4e7.mongodb.net/ecommerce"
-    },
-    github:{
-        clientId:"Iv1.678adf8aec4b77de",
-        clienteSecret:"1c15f4c29881e12f1990e98a872e97d9bcfa73d2",
-        callbackUrl:"http://localhost:8080/api/session/githubCallback"
-    }
+require('dotenv').config();
+
+export default {
+    port: process.env.PORT,
+    mongoUrl: process.env.MONGO_URL,
+    secretSession: process.env.SECRET,
+
 }
+
+
+// export const config = {
+//     server:{
+//         port:8080,
+//         secretSession:"CoderSecret"
+//     },
+//     fileSystem:{
+//         productsFile:"products.json",
+//         cartFile:"carts.json"
+//     },
+//     mongo:{
+//         url:"mongodb+srv://PracticaIntegradora:12344@cursobackend.aoxi4e7.mongodb.net/ecommerce"
+//     },
+//     github:{
+//         clientId:"Iv1.678adf8aec4b77de",
+//         clienteSecret:"1c15f4c29881e12f1990e98a872e97d9bcfa73d2",
+//         callbackUrl:"http://localhost:8080/api/session/githubCallback"
+//     }
+// }
