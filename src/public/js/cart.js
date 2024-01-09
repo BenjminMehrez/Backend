8e04d19187d2e17eec0abb815fdfd94bf5d622e8
Benjamin Mehrez <benjaminjofre01@gmail.com>
2024-01-09 15:41:58 -0300

    Trabajo final

--- src/public/js/cart.js
+++ src/public/js/cart.js
@@ -8,18 +8,18 @@ function updateCartList(products) {
   } else {
     products.forEach((product) => {
       cartContent += `
-    <div class="cart-item">
-    <img src="${product.product.thumbnail}" class="cart-item-image">
-    <div class="cart-item-details">
-    <h5 class="cart-item-title">${product.product.title}</h5>
-    <p class="cart-item-category">Categoría: ${product.product.category}</p>
-    <p class="cart-item-id">ID: ${product.product._id}</p>
-    <p class="cart-item-description">Descripción: ${product.product.description}</p>
-    <p class="cart-item-price">$${product.product.price}</p>
-    </div>
-    <p class="cart-item-quantity">Cantidad: ${product.quantity}</p>
-    <button data-cid="${cartId}" data-pid="${product.product._id}" class="remove-from-cart" onclick="removeProductFromCart('${cartId}', '${product.product._id}')">Eliminar</button>
-    </div>
+  <div class="card" style="width: 18rem;">
+      <img src="${product.product.thumbnail}" class="card-img-top">
+        <div class="card-body">
+          <h5 class="card-title">${product.product.title}</h5>
+          <p class="cart-item-category">Categoría: ${product.product.category}</p>
+          <p class="cart-item-id">ID: ${product.product._id}</p>
+          <p class="cart-text">Descripción: ${product.product.description}</p>
+          <p class="cart-item-price">$${product.product.price}</p>
+        </div>
+        <p class="cart-item-quantity">Cantidad: ${product.quantity}</p>
+        <button data-cid="${cartId}" data-pid="${product.product._id}" class="remove-from-cart" onclick="removeProductFromCart('${cartId}', '${product.product._id}')">Eliminar</button>
+  </div>
     `
         ;
     });
